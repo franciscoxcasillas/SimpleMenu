a8b31e9e620b9ec2afbddeea2cd8af9ba08532d3
Francisco Casillas <chiccasillas@gmail.com>
2025-02-04 12:14:51 -0600

    Initial Commit

---
+++ SimpleMenu/SimpleMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMenuApp.swift
+//  SimpleMenu
+//
+//  Created by Francisco Jean on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
