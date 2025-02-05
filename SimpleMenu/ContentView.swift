a279efb1db99635b9499f7f01f24b5c8b4e24458
Francisco Casillas <chiccasillas@gmail.com>
2025-02-05 13:56:09 -0600

    Improve fonts styling and image sizes. Fix spacing and add buttons.

--- SimpleMenu/ContentView.swift
+++ SimpleMenu/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Color.yellow.opacity(0.1).ignoresSafeArea(.all)
             VStack {
                 Header()
+                Title()
                 Menu()
+                RestaurantFooter()
             }
         }
     }
@@ -21,15 +23,32 @@ struct ContentView: View {
 
 struct Header: View {
     var body: some View {
-        VStack {
-            Text("Crabs and Burgers")
-                .font(.system(size: 45))
-            Text("The best sea food in town")
-                .font(.system(size: 20))
+        HStack {
+            Image(systemName: "person.circle.fill")
+            Spacer()
+            Image(systemName: "cart.fill")
         }
-        .padding()
-        .cornerRadius(10)
-        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 30)
+        .font(.system(size: 25))
+    }
+}
+
+struct Title: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Text("Crabs and Burgers")
+                    .font(.custom("Noteworthy", size: 45))
+                    .foregroundColor(.black)
+                    .offset(x: 1, y: 1)
+                Text("Crabs and Burgers")
+                    .font(.custom("Noteworthy", size: 45))
+                    .foregroundColor(.red.opacity(0.7))
+                        }
+            Text("The best sea food in town")
+                .font(.custom("Rockwell", size: 16))
+        }
+        .padding(.top, 5)
     }
 }
 
@@ -37,46 +56,105 @@ struct Menu: View {
     var body: some View {
         VStack {
             HStack {
-                Image("Burger")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Burger")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
                 Spacer().frame(width: 15)
-                Image("Fish")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Fish")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
             }
             HStack {
-                Image("Shrimp")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Shrimp")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
                 Spacer().frame(width: 15)
-                Image("Lobster")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Lobster")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
             }
             HStack {
-                Image("Taco")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Taco")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
                 Spacer().frame(width: 15)
-                Image("Pasta")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(20)
-                    .shadow(radius: 3)
+                VStack {
+                    Image("Pasta")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .shadow(radius: 3)
+                    Buttons()
+                }
             }
         }
-        .padding(25)
+        .padding(.horizontal, 25)
+        .padding(.top, 15)
+        .padding(.bottom, 22)
+    }
+}
+
+struct Buttons: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                print("Added to shopping cart")
+            }) {
+                Text("Add")
+                    .font(.custom("Andale Mono", size: 15))
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 3)
+                    .foregroundColor(.white)
+                    .background(Color.orange)
+                    .cornerRadius(15)
+            }
+            Button(action: {
+                print("Added to favorite")
+            }) {
+                Text("Favorite")
+                    .font(.custom("Andale Mono", size: 15))
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 3)
+                    .foregroundColor(.white)
+                    .background(Color.blue.opacity(0.7))
+                    .cornerRadius(15)
+            }
+        }
+    }
+}
+
+struct RestaurantFooter: View {
+    var body: some View {
+        HStack {
+            Text("All rights reserved")
+            Text("2025")
+        }
+        .font(.custom("Arial", size: 13))
     }
 }
 
